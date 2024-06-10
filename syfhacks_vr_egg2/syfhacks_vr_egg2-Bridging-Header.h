a9d91c4b480e33bb513178bdd5cc46b7f85a6a89
Shubham More <shubmore1999@gmail.com>
2024-06-10 17:47:24 -0500

    Initial Commit

---
+++ syfhacks_vr_egg2/syfhacks_vr_egg2-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  syfhacks_vr_egg2-Bridging-Header.h
+//  syfhacks_vr_egg2
+//
+//  Created by Shubham  More on 6/10/24.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
